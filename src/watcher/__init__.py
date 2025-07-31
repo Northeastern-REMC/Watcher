48ec347b788588c1f9275680279996ee8c66307e
kc-nremc <k_courtney@nremc.com>
2025-07-31 10:41:23 -0400

    Added in basic ignite thing

--- src/watcher/__init__.py
+++ src/watcher/__init__.py
@@ -1,2 +1,12 @@
-def main() -> None:
-    print("Hello from watcher!")
+from flask import Flask
+
+
+def ignite() -> Flask:
+    """Driver function to start the Watcher application
+
+    Returns:
+        Flask: Flask application with all attached routes and middleware
+    """
+    app = Flask(__name__)
+    
+    return app
