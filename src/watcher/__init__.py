cd2c1b7885bd6a958d9aa11638bb797ef3ed2565
kc-nremc <k_courtney@nremc.com>
2025-07-31 11:47:16 -0400

    Added in db support

--- src/watcher/__init__.py
+++ src/watcher/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from sqlalchemy import URL
 from watcher import home
+from watcher.db import db
+import os
+from dotenv import load_dotenv
+from urllib import parse
 
 
 def ignite() -> Flask:
@@ -9,6 +14,22 @@ def ignite() -> Flask:
         Flask: Flask application with all attached routes and middleware
     """
     app = Flask(__name__)
+    load_dotenv()
+    connection_url = URL.create(
+        "mssql+pyodbc",
+        username=os.environ["WATCHER_DB_USERNAME"],
+        password=os.environ["WATCHER_DB_PASSWORD"],
+        host=os.environ["WATCHER_DB_HOST"],
+        port=int(os.environ["WATCHER_DB_PORT"]),
+        database=os.environ["WATCHER_DB_NAME"],
+        query={
+            "driver": "ODBC Driver 18 for SQL Server",
+            "TrustServerCertificate": "yes"
+        },
+    )
+    app.config["SQLALCHEMY_DATABASE_URI"] = connection_url
+    
+    db.init_app(app)
     
     app.register_blueprint(home.bp)
     
